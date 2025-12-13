7b2a27973a34a18d9dd32c80e1f2825aa44b5c1f
Pedro Rocha <jpnr79@gmail.com>
2025-12-13 04:00:08 +0000

    11

--- inc/profile.class.php
+++ inc/profile.class.php
@@ -101,7 +101,7 @@ class PluginIframeProfile extends Profile {
     * @param $items_id integer id of the profile
     * @param $target value url of target
     *
-    * @return nothing
+   * @return void
     **/
    function showForm($profiles_id=0, $openform=TRUE, $closeform=TRUE) {
 
@@ -131,6 +131,7 @@ class PluginIframeProfile extends Profile {
          Html::closeForm();
       }
       echo "</div>";
+      return;
    }
 
    static function getAllRights($all = false) {
